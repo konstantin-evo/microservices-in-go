d61b544950375b3776684251edc7003d16ef8599
Konstantin.Priluchnyi@telekom.com <Konstantin.Priluchnyi@telekom.com>
2023-03-24 11:00:20 +0200

    Set up an RPC Server in the Logger service

--- logging-service/cmd/api/main.go
+++ logging-service/cmd/api/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"log-service/data"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -48,6 +50,10 @@ func main() {
 		Models: data.New(client),
 	}
 
+	// Register RPC Server
+	err = rpc.Register(new(RPCServer))
+	go app.rpcListen()
+
 	// start web server
 	log.Println("Starting service on port", webPort)
 	srv := &http.Server{
@@ -60,6 +66,24 @@ func main() {
 		log.Panic()
 	}
 
+}
+
+func (app *Config) rpcListen() error {
+	log.Println("Starting RPC server on port ", rpcPort)
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return err
+	}
+	defer listen.Close()
+
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+
+		go rpc.ServeConn(rpcConn)
+	}
 }
 
 func connectToMongo() (*mongo.Client, error) {
