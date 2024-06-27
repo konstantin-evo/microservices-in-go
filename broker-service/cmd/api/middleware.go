983a32849cf53c0ef121c2e90ea4d88ef69ba47d
konstantin_evo <konstantin.priluchnyi@gmail.com>
2024-06-27 12:29:14 +0300

    Broker: refactor (change chi router with gin)

--- broker-service/cmd/api/middleware.go
+++ broker-service/cmd/api/middleware.go
@@ -2,29 +2,33 @@ package main
 
 import (
 	"bytes"
+	"github.com/gin-gonic/gin"
 	"io"
 	"log"
-	"net/http"
 	"time"
 )
 
-func LoggerMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
+func LoggerMiddleware(c *gin.Context) {
+	start := time.Now()
 
-		bodyBytes, _ := io.ReadAll(r.Body)
-		r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
-		log.Printf("Incoming request: %s %s %s\nBody: %s", r.Method, r.RequestURI, r.RemoteAddr, string(bodyBytes))
+	// Read the body
+	bodyBytes, _ := io.ReadAll(c.Request.Body)
+	c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+	log.Printf("Incoming request: %s %s %s\nBody: %s", c.Request.Method, c.Request.RequestURI, c.ClientIP(), string(bodyBytes))
 
-		ww := &responseWriter{ResponseWriter: w, body: new(bytes.Buffer)}
-		next.ServeHTTP(ww, r)
+	// Use a custom response writer to capture the response body
+	ww := &responseWriter{ResponseWriter: c.Writer, body: new(bytes.Buffer)}
+	c.Writer = ww
 
-		log.Printf("Response: %d %s %s %s %s\nBody: %s", ww.status, r.Method, r.RequestURI, r.RemoteAddr, time.Since(start), ww.body.String())
-	})
+	// Process request
+	c.Next()
+
+	// Log the response
+	log.Printf("Response: %d %s %s %s %s\nBody: %s", ww.status, c.Request.Method, c.Request.RequestURI, c.ClientIP(), time.Since(start), ww.body.String())
 }
 
 type responseWriter struct {
-	http.ResponseWriter
+	gin.ResponseWriter
 	status int
 	body   *bytes.Buffer
 }
@@ -37,4 +41,9 @@ func (rw *responseWriter) WriteHeader(status int) {
 func (rw *responseWriter) Write(b []byte) (int, error) {
 	rw.body.Write(b)
 	return rw.ResponseWriter.Write(b)
+}
+
+func (rw *responseWriter) WriteString(s string) (int, error) {
+	rw.body.WriteString(s)
+	return rw.ResponseWriter.WriteString(s)
 }
--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -1,31 +1,58 @@
 package main
 
 import (
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/penglongli/gin-metrics/ginmetrics"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
+	"time"
 )
 
 func (app *Config) routes() http.Handler {
-	mux := chi.NewRouter()
+	r := gin.Default()
 
-	// specify who is allowed to connect
-	mux.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
+	// CORS configuration
+	config := cors.Config{
+		AllowOrigins:     []string{"https://*", "http://*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposeHeaders:    []string{"Link"},
 		AllowCredentials: true,
-		MaxAge:           300,
-	}))
+		MaxAge:           12 * time.Hour,
+	}
+	r.Use(cors.New(config))
 
-	mux.Use(middleware.Heartbeat("/ping"))
-	mux.Use(LoggerMiddleware)
+	// Heartbeat endpoint
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "OK")
+	})
 
-	mux.Post("/handle", app.HandleSubmission)
-	mux.Handle("/metrics", promhttp.Handler())
+	// Prometheus metrics endpoint
+	// get global Monitor object
+	m := ginmetrics.GetMonitor()
 
-	return mux
+	// +optional set metric path, default /debug/metrics
+	m.SetMetricPath("/metrics")
+	// +optional set slow time, default 5s
+	m.SetSlowTime(10)
+	// +optional set request duration, default {0.1, 0.3, 1.2, 5, 10}
+	// used to p95, p99
+	m.SetDuration([]float64{0.1, 0.3, 1.2, 5, 10})
+
+	// set middleware for gin
+	m.Use(r)
+
+	// Custom logger middleware
+	r.Use(LoggerMiddleware)
+
+	// HandleSubmission endpoint
+	r.POST("/handle", func(c *gin.Context) {
+		app.HandleSubmission(c.Writer, c.Request)
+	})
+
+	// Prometheus' metrics endpoint
+	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
+
+	return r
 }
